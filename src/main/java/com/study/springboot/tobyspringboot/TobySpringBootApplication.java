438f29f66a8eb4c1f4804e88449b828d3a761565
wontaejo <wontaejo87@gmail.com>
2023-01-20 23:39:01 +0900

    first commit

---
+++ src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot.tobyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springboot/tobyspringboot/TobySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot.tobyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
