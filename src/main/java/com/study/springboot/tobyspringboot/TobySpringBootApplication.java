6f5c398ab9d10ad38de5fd80610b4312dca78baf
wontaejo <wontaejo87@gmail.com>
2023-01-21 22:44:44 +0900

    서블릿 컨테이너 띄우기 : TomcatServletWebServerFactory, WebServer 이용해 톰캣 서블릿 컨테이너 실해

--- src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
@@ -1,9 +1,17 @@
 package com.study.springboot.tobyspringboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerless Standalone Application");
+        // 스프링 부트가 톰캣 서블릿 컨테이너를 내장하여 쉽게 시작할 수 있게 만들어 놓은 도우미 클레스
+        // Factory 복잡한 설정 과정을 거쳐서 TomcatServletWebServerFactory 만들어준다
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start(); // 톰캣 서블릿 컨테이너가 실행된다.
     }
 
 }
