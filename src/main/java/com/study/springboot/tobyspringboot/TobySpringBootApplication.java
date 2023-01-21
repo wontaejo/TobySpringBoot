5b5e07dedce6ed91514ff84cf8dc3edf41bc4ea7
wontaejo <wontaejo87@gmail.com>
2023-01-21 22:05:36 +0900

    Containerless 개발 준비 : SpringBoot 를 사용하지 않고, HelloController 를 동작 시키기 위해 SpringBoot 동작 코드 제거

--- src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
@@ -1,13 +1,9 @@
 package com.study.springboot.tobyspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringBootApplication.class, args);
+        System.out.println("Hello Containerless Standalone Application");
     }
 
 }
