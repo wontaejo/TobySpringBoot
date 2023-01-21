04b91a372bf412e110b655a85d4194acc72fb955
wontaejo <wontaejo87@gmail.com>
2023-01-21 23:40:09 +0900

    Hello 컨트롤러 매핑과 바인딩 : 매핑과 방인 처리 추가

--- src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
@@ -21,16 +21,22 @@ public class TobySpringBootApplication {
         // Factory 복잡한 설정 과정을 거쳐서 TomcatServletWebServerFactory 만들어준다
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController(); // 서블릿이 초기화될때 한번만 선언이 되면 된다.
+
            servletContext.addServlet("frontController", new HttpServlet() { // 처리하겠다
                @Override
                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                    // 인증, 보안, 다국어, 공통 기능
+
+                   // 매핑 : 웹요청에 있는 정보를 활용하여 어떤 요청을 호출할 것인지 선택.
                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                        String name = req.getParameter("name");
 
+                       String hello = helloController.hello(name); // 바인딩 : request 정보를 추출하여 처리하는 오브젝트로 파라미터로 넘겨주는것
+
                        resp.setStatus(HttpStatus.OK.value());
                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                       resp.getWriter().println("Hello " + name);
+                       resp.getWriter().println(hello);
                    } else if (req.getRequestURI().equals("/user")) {
                        //
                    } else {
