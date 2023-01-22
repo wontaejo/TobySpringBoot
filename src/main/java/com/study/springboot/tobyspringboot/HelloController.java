199cb5bbf79be2b8cf328a502badf134ee2f06cc
wontaejo <wontaejo87@gmail.com>
2023-01-22 22:31:05 +0900

    의존 오브젝트 추가 : SimpleHelloService 를 생성하여, Controller 와 역할 분리

--- src/main/java/com/study/springboot/tobyspringboot/HelloController.java
+++ src/main/java/com/study/springboot/tobyspringboot/HelloController.java
@@ -1,7 +1,12 @@
 package com.study.springboot.tobyspringboot;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/com/study/springboot/tobyspringboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.study.springboot.tobyspringboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
