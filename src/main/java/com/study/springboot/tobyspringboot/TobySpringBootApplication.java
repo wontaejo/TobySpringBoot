98bc43ca0deaa00f342538ee9e1fe5dd7f8bc6bd
wontaejo <wontaejo87@gmail.com>
2023-01-26 00:09:37 +0900

    합성 애노테이션의 적용 : MySpringBooAnnotation, Config 를 통해  Spring 초기 셋팅 시의 Application 과 보이는 구조는 같아짐

--- src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/study/springboot/tobyspringboot/TobySpringBootApplication.java
@@ -1,26 +1,8 @@
 package com.study.springboot.tobyspringboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
-
-@Configuration // 구성 정보를 가지고 있는 class 다.
-@ComponentScan // 컴포넌트가 붙은 클레스를 찾아서 Bean 으로 등록해 달라 Scan 패키지 부터 하위 패키지까지 찾아서 빈으로 등록해준다.
+@MySpringBootAnnotation
 public class TobySpringBootApplication {
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(TobySpringBootApplication.class, args);
     }
--- src/test/java/com/study/springboot/tobyspringboot/HelloServiceTest.java
+++ src/test/java/com/study/springboot/tobyspringboot/HelloServiceTest.java
@@ -3,25 +3,8 @@ package com.study.springboot.tobyspringboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@UnitTest
-@interface FastUnitTest {
-}
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
-@Test
-@interface UnitTest {
-}
-
 public class HelloServiceTest {
-    @UnitTest
+    @Test
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
