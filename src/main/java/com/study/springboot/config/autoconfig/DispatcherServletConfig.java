74318574a812c5c32966540113258049785445d3
wontaejo <wontaejo87@gmail.com>
2023-01-26 23:00:20 +0900

    자동 구성 애노테이션 적용

--- src/main/java/com/study/springboot/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/com/study/springboot/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package com.study.springboot.config.autoconfig;
 
+import com.study.springboot.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
