1908b2f4eeb325804b62bdfac1a1f97eb19f3fb8
wontaejo <wontaejo87@gmail.com>
2023-01-28 00:00:40 +0900

    스타터와 Jetty 서버 구성 추가 : Jetty dependencies + 설정 파일 추가

--- src/main/java/com/study/springboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/study/springboot/config/autoconfig/JettyWebServerConfig.java
@@ -1,15 +1,16 @@
 package com.study.springboot.config.autoconfig;
 
 import com.study.springboot.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
 }
