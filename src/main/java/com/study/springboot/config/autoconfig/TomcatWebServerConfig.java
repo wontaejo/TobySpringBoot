55d3b5f860deea2a1258c5df749687b22c0fbca2
wontaejo <wontaejo87@gmail.com>
2023-01-27 23:56:29 +0900

    Tomcat -> Jetty 로 변경

--- src/main/java/com/study/springboot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/study/springboot/config/autoconfig/TomcatWebServerConfig.java
@@ -1,7 +1,7 @@
 package com.study.springboot.config.autoconfig;
 
 import com.study.springboot.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
 }
