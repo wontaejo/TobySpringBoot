c446b61d8c1d61059c1cb425b2dc684e9f1f2b4f
wontaejo <wontaejo87@gmail.com>
2023-01-29 22:10:01 +0900

    자동 구성에 Environment 프로퍼티 적용 : TomcatWebServerConfig Evnironment 를 통해 contextPath 프로퍼티 적용

--- src/main/java/com/study/springboot/TobySpringBootApplication.java
+++ src/main/java/com/study/springboot/TobySpringBootApplication.java
@@ -9,14 +9,6 @@ import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class TobySpringBootApplication {
-    @Bean
-    ApplicationRunner applicationRunner(Environment env) {
-        return args -> {
-           String name = env.getProperty("my.name");
-           System.out.println("my.name: " + name);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(TobySpringBootApplication.class, args);
     }
